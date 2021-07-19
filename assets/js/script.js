ee2400207842deff9457dad6f9697130feb2acfb
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-07-19 17:05:40 -0400

    initial web page and dependent assets, started js

---
+++ assets/js/script.js
@@ -0,0 +1,90 @@
+//Data (global variables, things to keep track of)=====================
+//question1 object
+question1: {
+    q: "Commonly used data types DO NOT include:",
+    choice1: "strings",
+    choice2: "booleans",
+    choice3: "alerts",
+    choice4: "numbers"
+}
+
+question2: {
+    q: "The condition in an if/else statement is enclosed within _____.",
+    choice1: "quotes",
+    choice2: "curly brackets",
+    choice3: "parentheses",
+    choice4: "square brackets"
+}
+
+question3: {
+    q: "Arrays in JavaScript can be used to store _____.",
+    choice1: "numbers and strings",
+    choice2: "other arrays",
+    choice3: "booleans",
+    choice4: "all the above"
+}
+
+question4: {
+    q: "String values must be enclosed within _____ when being assigned to variables.",
+    choice1: "commas",
+    choice2: "curly brackets",
+    choice3: "quotes",
+    choice4: "parentheses"
+}
+
+question5: {
+    q: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choice1: "JavaScript",
+    choice2: "terminal/bash",
+    choice3: "for loops",
+    choice4: "console.log"
+}
+
+//answer indicator, finished playing, final score message, enter initials
+var rightAnswer: "Correct!";
+var wrongAnswer: "Wrong!";
+var finishedPlaying: "All done!";
+var finalScoreMsg: "Your final score is ";
+var enterInitials: "Enter initials:";
+
+
+//timer
+var timer: //number;
+
+//score
+var score: //number;
+
+//highscore
+var highScoresMsg: "Highscores";
+var highScoresLinkText: "Highscores";
+
+
+//Functions============================================================
+    //
+    //
+    //
+    //
+//User Interactions====================================================
+    //prompt to start quiz
+    //show question & options/choices
+    //show highscores link upper left corner
+    //show timer upper right corner
+    //timer counts down
+    //show Wrong! or Correct! after user selects or times runs out on each question
+    //show All done! message when all questions answered or times runs out
+    //show final score
+    //show form input field for initials
+    //ask for initials to be entered
+    //include Submit button
+
+    //display highscores/leaderboard
+        //apply styles to leaderboard
+        //(ex: 1. Name - ### where ### is score)
+        //inlude Go Back button option
+        //incluse Clear Highscores button option
+    
+    //hide highscores link upper left corner
+    //hide timer upper right corner
+//Initialization=======================================================
+    //
+    //
